d55942214613b6a4a7a97d1ebec22ad5ab389d5a
VG <vengadoravg@gmail.com>
2020-01-11 21:53:33 -0400

    waiter is going for driiiinks xD

--- Assets/Scripts/YesNoDetector.cs
+++ Assets/Scripts/YesNoDetector.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class YesNoDetector : MonoBehaviour {
   public event System.Action<YesNoDetector, bool> onDetected;
 
+  public Transform head;
   public AngularTracker tracker;
   public GenericShakeDetector yes;
   public GenericShakeDetector no;
@@ -21,25 +22,28 @@ public class YesNoDetector : MonoBehaviour {
 
   void Reset () {
     tracker = GetComponent<AngularTracker>();
-    yes.Reset(this.transform);
-    no.Reset(this.transform);
+    yes.Reset(head.transform);
+    no.Reset(head.transform);
   }
 
   void Update () {
     Vector3 forward =
-      transform.InverseTransformPoint(transform.position + yes.last.forward);
+      head.transform.InverseTransformPoint(head.transform.position + yes.last.forward);
     Vector3 up =
-      transform.InverseTransformPoint(transform.position + yes.last.up);
+      head.transform.InverseTransformPoint(head.transform.position + yes.last.up);
     yes.Update(tracker.velocity.x,
                Mathf.Abs(forward.x), Mathf.Abs(up.x), Mathf.Abs(forward.y));
 
-    forward = transform.InverseTransformPoint(transform.position + no.last.forward);
-    up = transform.InverseTransformPoint(transform.position + no.last.up);
+    forward = head.transform.InverseTransformPoint(head.transform.position +
+                                                   no.last.forward);
+    up = head.transform.InverseTransformPoint(head.transform.position +
+                                              no.last.up);
     no.Update(tracker.velocity.y,
               Mathf.Abs(forward.y), Mathf.Abs(up.x), Mathf.Abs(forward.x));
   }
 
   public void HandleShake (GenericShakeDetector caller) {
     if (onDetected != null) onDetected(this, caller == yes);
+    print(caller == yes? "yes detected": "no detected");
   }
 }
---
+++ Assets/Scripts/YesNoQuestion.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class YesNoQuestion : MonoBehaviour, IScriptPiece {
+  public event System.Action onFinished;
+
+  public string scriptName;
+  public float waitForAnswer;
+  public int counter;
+  public YesNoDetector player;
+  public GameObject yes;
+  public GameObject no;
+  public GameObject silence;
+
+  public void Execute () {
+    StartListening();
+  }
+
+  void Reset () {
+    VoiceScript voice = GetComponent<VoiceScript>();
+    if (voice) {
+      waitForAnswer = voice.clip.length;
+    }
+  }
+
+  void StartListening () {
+    player.onDetected += AnswerHandler;
+    StartCoroutine(_WaitForAnswer());
+  }
+
+  void StopListening () {
+    player.onDetected -= AnswerHandler;
+    GameObject target;
+    if (counter > 0) {
+      target = yes;
+    } else if (counter < 0) {
+      target = no;
+    } else {
+      target = silence;
+    }
+    target.SetActive(true);
+    target.transform.parent = transform.parent;
+    target.name = scriptName;
+  }
+
+  IEnumerator _WaitForAnswer () {
+    yield return new WaitForSeconds(waitForAnswer);
+    StopListening();
+  }
+
+  public void AnswerHandler (YesNoDetector caller, bool value) {
+    counter += value? 1: -1;
+  }
+}
